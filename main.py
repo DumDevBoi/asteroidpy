a7668f42e6cf3c6e244e6913df07cfb960086262
DumDevBoi <daryl@LAPTOP-KPPELF97.>
2026-01-17 12:35:53 +0800

    turning

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     print("Hello from asteroidpy!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while(1):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.update(dt)
+        player1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
